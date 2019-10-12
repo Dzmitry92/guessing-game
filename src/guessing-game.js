c87fe7f2e55c9a4a37766f81bbd8ff03c02a22fb
Dmitry <teramisu01@gmail.com>
2019-10-12 18:50:27 +0300

    feat: implement guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,38 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.tempNumber = null;
+        this.minIndex = null
+        this.maxIndex = null
+    }
 
     setRange(min, max) {
-
+        this.minIndex = min;
+        this.maxIndex = max;
+        this.array = [];
+        for (var i = min; i <= max ; i++) {
+            this.array.push(i);
+        }
     }
 
     guess() {
-
+        this.tempNumber = this.array[Math.floor(this.array.length/2)];				
+        return this.tempNumber;
     }
 
     lower() {
-
+        this.array = [];
+        for (var i = this.minIndex; i <= this.tempNumber; i++) {
+            this.array.push(i);
+        }
+        this.maxIndex = this.array[this.array.length - 1];
     }
 
     greater() {
-
+        this.array = [];
+        for (var i = this.tempNumber; i <= this.maxIndex; i++) {
+            this.array.push(i);
+        }
+        this.minIndex = this.array[0];
     }
 }
 
